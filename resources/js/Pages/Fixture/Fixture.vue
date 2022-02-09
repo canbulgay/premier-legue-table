1e806d64ff42d48dc4b06e036b04477b445be5e9
canbulgay <canbulgay@outlook.com>
2022-02-10 01:38:52 +0300

    Create button and method to retrieve data from MatchController

--- resources/js/Pages/Fixture/Fixture.vue
+++ resources/js/Pages/Fixture/Fixture.vue
@@ -4,19 +4,19 @@
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Fixture
             </h2>
-            <div>
-                <button class="bg-blue-500 shadow-lg shadow-blue-500/50 ...">Set Game</button>
+            <div class="mt-5 ">
+                <button  class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" @click.prevent="setNextWeekGames" >Set Next Week</button>
             </div>
 
         </template>
 
         <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8 flex justify-center">
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <div v-if="fixture > 0" class="p-6 bg-white border-b border-gray-200">
                         <table>
                             <thead class="font-bold bg-gray-300 border-b-2">
-                                <td class="px-4 py-2">Match Id</td>
+                                <td class="px-4 py-2">#</td>
                                 <td class="px-4 py-2"><p class="text-right">Home Team</p></td> 
                                 <td class="px-4 py-2">|</td> 
                                 <td class="px-4 py-2">Away Team</td>
@@ -67,7 +67,7 @@
 </template>
 
 <script>
-    import { defineComponent } from 'vue'
+    import { defineComponent} from 'vue'
     import AppLayout from '@/Layouts/AppLayout.vue'
 
     export default defineComponent({
@@ -78,7 +78,15 @@
         computed : {
             fixture(){
                 return this.fixtures.length;
-            }
+            },
+        },
+        methods : {
+            setNextWeekGames(){
+                fetch('/nextWeek')
+                .then((response) => response.json())
+                .catch((err) => console.log(err));
+                window.location.reload();
+            },
         }
     })
 </script>
